934e6fabe4cf2d0ef86a377219d4dc9ba7cc9cf7
BottomSheet <k.vashurkin@bk.ru>
2026-06-19 01:46:18 +0300

    fix: apply Nunito to all headings

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/pitipiwpiw/',   // ← ОБЯЗАТЕЛЬНО должно быть именно так
+  base: '/pitipiwpiw/',  
 
   plugins: [
     vue(),
